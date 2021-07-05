75e3cab7cda9087820441ca4951e76892826d581
MS-MS-MS <13439718124@163.com>
2021-07-05 18:32:25 +0800

    appium webview

---
+++ Test_appium/test_webview.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+""" 
+@Time    : 2021/7/5 15:18
+@Author  : MaSai
+@FileName: test_webview.py
+@SoftWare: PyCharm
+"""
+from appium import webdriver
+
+"""
+android webview 练习
+"""
+class Test_webview:
+    def setup(self):
+        caps={}
+        caps['platformName'] ='android'
+        caps['platformVersion'] = '7.1'
+        caps['deviceName'] = '127.0.0.1:21503'
+        caps['browserName'] = 'Chrome'
+        # 调用工作引擎 安卓已经默认调用了
+        caps['automationName']= 'uiautomator2'
+        caps["noReset"] = 'true'
+        caps["skipDeviceInitialization"] = 'true'
+        # chromedriverExecutable 指定chromewebdriver 路径
+        caps['chromedriverExecutable']='D:\chromewebdriver\chromedriver\chromedriver.exe'
+        self.driver=webdriver.Remote("http://127.0.0.1:4723/wd/hub",caps)
+        self.driver.implicitly_wait(5)
+
+    def treadown(self):
+        pass
+    def test_browser(self):
+        self.driver.get("http://m.baidu.com")
