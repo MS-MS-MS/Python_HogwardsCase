8d8416500be5bf2a8579b81c70e4921f2645c144
MS-MS-MS <13439718124@163.com>
2021-07-05 14:41:19 +0800

    appium dome

--- Test_appium/test_appdome.py
+++ Test_appium/test_appdome.py
@@ -14,8 +14,7 @@
 # desired_caps['appPackage']='com.xueqiu.android'
 # desired_caps['appActivity']='.view.WelcomeActivityAlias'
 # driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
-
-
+import yaml
 from appium import webdriver
 # desired_caps={}
 # desired_caps['platformName']='Android'
@@ -51,13 +50,26 @@ from appium import webdriver
 # desired_caps['appActivity']='com.android.settings.Settings'
 # driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
 
-desired_caps={}
-desired_caps['platformName']='Android'
-desired_caps['platformVersion']='7.1'
-desired_caps['deviceName']='127.0.0.1:21503'
-# com.android.settings/com.android.settings.Settings
-desired_caps['appPackage'] = 'com.xueqiu.android'
-desired_caps['appActivity'] = '.view.WelcomeActivityAlias'
-desired_caps["noReset"] = 'true'
-driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
-print("启动【设置】应用")
+# desired_caps={}
+# desired_caps['platformName']='Android'
+# desired_caps['platformVersion']='7.1'
+# desired_caps['deviceName']='127.0.0.1:21503'
+# # com.android.settings/com.android.settings.Settings
+# # desired_caps['appPackage'] = 'com.xueqiu.android'
+# # desired_caps['appActivity'] = '.view.WelcomeActivityAlias'
+# desired_caps['appPackage'] = 'io.appium.android.apis'
+# desired_caps['appActivity'] = ' io.appium.android.apis.view.PopupMenu1'
+# desired_caps["noReset"] = 'true'
+# driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+# # print("启动【设置】应用")
+
+
+def get_yaml():
+    with open(r"YAML.yaml")as f:
+        date = yaml.safe_load(f)
+        # print(date)
+        xq = date['date']['code']
+        ids = date['date']['ids']
+        print(xq,ids)
+        return [xq, ids]
+get_yaml()
